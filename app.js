b9f9a8953cfd8b95edfbbee2d15797f58824f2b5
Stathis <lagstathis@gmail.com>
2022-09-27 19:37:19 +0300

    Second Commit

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Hello Js world");
+// let computerAnwser = null;
+
+function getComputerChoice() {
+  choice = ["Rock", "Paper", "Scissor"];
+  let selection = choice[Math.floor(Math.random() * choice.length)];
+  return selection;
+}
+
+console.log(getComputerChoice());
